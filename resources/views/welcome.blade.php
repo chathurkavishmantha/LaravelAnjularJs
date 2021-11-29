737eacc5320953a5d73d6b63e582687190fcffd1
chathurkavishmantha <chathurkavish@gmail.com>
2021-11-29 12:52:19 +0530

    add insert funtion to successfully add data to the database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,6 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
-                        <a href="{{ url('/view-student') }}">Students</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
@@ -84,7 +83,7 @@
 
             <div class="content" ng-app="myApp" ng-controller="myCtrl" ng-init="list()">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 6
                 </div>
                 
 
@@ -107,6 +106,65 @@
 
         </div>
 
-      
+
+        
+
+
+        <script type="text/javascript">
+            var app = angular.module('myApp',[]);
+            //controller
+            app.controller('myCtrl', function($scope,$http){
+                $scope.save = function(){
+                    console.log($scope.name); //view consol results
+                    console.log($scope.description); //view consol results
+
+                    $http({
+
+                        url : "{{URL('api/insert')}}",
+                        method : "POST",
+                        data : {
+                            "name" : $scope.name,
+                            "description" : $scope.description
+                        }
+
+                        }).then(function(response){
+                            alert('success');
+                            $scope.list();
+                        },function(response){
+                            alert('failed');
+                        });
+
+                    
+                }
+
+                $scope.list = function(){
+                        $http.get("{{ URL('api/show' )}}")
+                        .then(function(response){
+                            $scope.tasks = response.data;
+                        });
+                }
+
+                $scope.edit_task = {};
+                // initialize update action
+                $scope.initEdit = function (index) {
+                    $scope.errors = [];
+                    $scope.edit_task = $scope.tasks[index];
+                    $("#edit_task").modal('show');
+                };
+
+                // update the given task
+                $scope.updateTask = function () {
+                    $http.patch('/task/' + $scope.edit_task.id, {
+                        name: $scope.edit_task.name,
+                        description: $scope.edit_task.description
+                    }).then(function success(e) {
+                        $scope.errors = [];
+                        $("#edit_task").modal('hide');
+                    }, function error(error) {
+                        $scope.recordErrors(error);
+                    });
+                };
+            });
+        </script>
     </body>
 </html>
